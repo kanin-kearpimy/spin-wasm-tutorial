0e8cf537c56964b5d2198eb169a29ff9fa36e237
kanin-kearpimy <kanin.kearpimy@gmail.com>
2023-03-16 16:31:36 +0700

    game start and scoreing

--- game-control/app.py
+++ game-control/app.py
@@ -1,8 +1,44 @@
 from spin_http import Response
-
+from spin_redis import redis_get, redis_set
+import json
+from os import environ
 
 def handle_request(request):
-
+    redisHost = environ.get('REDIS_HOST')
+    jsonObj = getJsonBody(request.body)
+    session_id = getKey(jsonObj, 'session_id')
+    answer = getKey(jsonObj, 'answer')
+    timeSlot = getKey(jsonObj, 'timeSlot')
+    session = redis_get(redisHost, session_id)
+    sessionJsonObj = json.loads(session)
+    if(checkAnswer(answer, timeSlot, getKey(sessionJsonObj, 'session'))): 
+        sessionJsonObj = json.dumps(increasePoint(sessionJsonObj)).encode('utf-8')
+        redis_set(redisHost, session_id, sessionJsonObj)
+    
     return Response(200,
                     [("content-type", "text/plain")],
-                    bytes(f"Hello from the Python SDK", "utf-8"))
+                    bytes(f"Successfully", "utf-8"))
+
+
+def getJsonBody(body):
+    if(body):
+        return json.loads(body)
+    return {}
+
+def hasKey(dictionary, key):
+    return key in dictionary.keys()
+
+def getKey(dictionary, key):
+    if(hasKey(dictionary, key)):
+        return dictionary[key]
+    return None
+
+def checkAnswer(answer, timeslot, session):
+    return session[timeslot] == answer
+
+def increasePoint(session):
+    point = int(getKey(session, 'score'))
+    point += 1
+    session['score'] = point
+    return session
+    
--- game-session/app.py
+++ game-session/app.py
@@ -1,5 +1,7 @@
 from spin_http import Response
-import random, json
+import random, json, uuid
+from spin_redis import redis_publish, redis_set
+from os import environ
 
 headers = [("content-type", "application/json")]
 status = 200
@@ -16,9 +18,19 @@ sessions = ["number-1",
     "next"]
 
 def handle_request(request):
-    jsonBody = json.loads(request.body)
-    totalSession = getKey(jsonBody, 'totalSession') if hasKey(jsonBody, 'totalSession') else 15
+    redisHost = environ.get('REDIS_HOST')
+    queryParms = getQueryParams(request.uri)
+    totalSession = int(getKey(queryParms, 'totalSession') if hasKey(queryParms, 'totalSession') else 15)
     session = generate_session(totalSession)
+    
+    session_id = str(uuid.uuid4())
+    sessionBody = json.dumps({
+        "session": session,
+        "score": 0
+    }).encode('utf-8')
+    
+    redis_set(redisHost, session_id, sessionBody)
+    
     responseBody = json.dumps({"session": session}).encode('utf-8')
     
     return Response(
@@ -39,3 +51,21 @@ def generate_session(totalSession):
     while(len(_session) < totalSession):
         _session.append(sessions[random.randint(0, 10)])
     return _session
+
+def getQueryParams(uri):
+    def map_key_value(text):
+        parser = text.split('=')
+        _key = parser[0]
+        _value = parser[1]
+        
+        return (_key, _value)
+
+    def convert_json(keyValueObj):
+        dictionary = {}
+        for key, value in keyValueObj:
+            dictionary[key] = value
+        return dictionary
+
+    keyValueObject = list(map(map_key_value, uri.split('?')[1].split('&')))
+    
+    return convert_json(keyValueObject)
